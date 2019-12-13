92086be2124680df5a25dfc41c21ae408666872a
pgruszczynski <pgruszczynski@empyrean.games>
2019-12-13 17:26:49 +0100

    Uploader update

--- Assets/Code/Editor/TinyPngUploader.cs
+++ Assets/Code/Editor/TinyPngUploader.cs
@@ -8,10 +8,14 @@ using UnityEngine;
 
 public class TinyPngUploader : EditorWindow {
     static string tinyPngApiKey;
+    static string texturePathsStr;
     static string[] texturesGUIDs;
 
     static bool isTexturesFoldoutClicked;
+    static bool areTexturesFound;
+    static int foundTexturesCount;
     static Vector2 scrollViewPos;
+    static Vector2 imgResolution;
 
     void OnGUI() {
         DrawLayout();
@@ -28,16 +32,17 @@ public class TinyPngUploader : EditorWindow {
         window.Show();
     }
 
-    void DrawAPIKeyField() {
+    void DrawInputFields() {
         
         tinyPngApiKey = EditorGUILayout.TextField("Tiny PNG API key", tinyPngApiKey);
         tinyPngApiKey = "Dh3sqdPbnmTgvXkxx7l1c1kPrTRg2c0S"; // hardcoded key
+        imgResolution = EditorGUILayout.Vector2Field("Width & height", imgResolution);
     }
 
     void DrawLayout() {
 
         EditorGUILayout.BeginVertical();
-        DrawAPIKeyField();
+        DrawInputFields();
         DrawTexturePathsFoldout();
         TryToDrawUploadImgLayout();
         TryToFindAllTextures();
@@ -46,24 +51,43 @@ public class TinyPngUploader : EditorWindow {
     }
 
     void DrawTexturePathsFoldout() {
-        scrollViewPos = EditorGUILayout.BeginScrollView(scrollViewPos);
+        scrollViewPos = EditorGUILayout.BeginScrollView(scrollViewPos, GUILayout.ExpandWidth(true), GUILayout.Height(300));
         isTexturesFoldoutClicked = EditorGUILayout.Foldout(isTexturesFoldoutClicked, "Texture asset paths:");
         if (isTexturesFoldoutClicked) {
-            GUILayout.Label(GetTexturesPathsString());
+            GUILayout.Label(texturePathsStr);
         }
+        EditorGUILayout.LabelField($"Textures found: {foundTexturesCount}");
         EditorGUILayout.EndScrollView();
     }
 
+    void TryToFindAllTextures() {
+        if (!GUILayout.Button("Find all textures GUIDs"))
+            return;
+
+        texturesGUIDs = AssetDatabase.FindAssets("t: Texture");
+        texturePathsStr = GetTexturesPathsString();
+    }
+    
     string GetTexturesPathsString() {
         if (texturesGUIDs == null || texturesGUIDs.Length == 0)
             return "No texture paths.";
 
         var strBuilder = new StringBuilder();
         for (var i = 0; i < texturesGUIDs.Length; i++) {
-            strBuilder.Append($"{AssetDatabase.GUIDToAssetPath(texturesGUIDs[i])}\n");
+            var filePath = AssetDatabase.GUIDToAssetPath(texturesGUIDs[i]);
+            if (!filePath.Contains(".jpg") && !filePath.Contains(".png"))
+                continue;
+            strBuilder.Append($"{GetAbsoluteTexturesPath(filePath)}\n");
+            ++foundTexturesCount;
         }
 
+        areTexturesFound = true;
+
         return strBuilder.ToString();
+    }
+
+    string GetAbsoluteTexturesPath(string filePath) {
+        return Path.Combine(Path.GetDirectoryName(Application.dataPath), filePath);
     }
     
     void TryToDrawUploadImgLayout() {
@@ -80,38 +104,13 @@ public class TinyPngUploader : EditorWindow {
         }
 
         using (var png = new TinyPngClient(tinyPngApiKey)) {
-            //Create a task to compress an image.
-            //this gives you the information about your image as stored by TinyPNG
-            //they don't give you the actual bits (as you may want to chain this with a resize
-            //operation without caring for the originally sized image).
             var compressImageTask =
                 png.Compress("C:\\Users\\range\\Desktop\\Textures_Test\\Originals\\FotelGamingowy_red tex albedo.png");
 
-            //If you want to actually save this compressed image off
-            //it will need to be downloaded 
             var compressedImage = await compressImageTask.Download();
-
-//            //you can then get the bytes
-//            var bytes = await compressedImage.GetImageByteData();
-//
-//            //get a stream instead
-//            var stream = await compressedImage.GetImageByteData();
-
-            //or just save to disk
             await compressedImage.SaveImageToDisk("C:\\Users\\range\\Desktop\\Textures_Test\\test2.png");
 
-            //Putting it all together
-//            await png.Compress("path")
-//                .Download()
-//                .SaveImageToDisk("savedPath");
         }
-    }
-
-    void TryToFindAllTextures() {
-        if (!GUILayout.Button("Find all textures GUIDs"))
-            return;
-
-        texturesGUIDs = AssetDatabase.FindAssets("t: Texture");
     }
 }
 
